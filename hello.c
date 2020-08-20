ecd02b31e435d4e6bb62f9547fb3eaafec4d9ead
Thanhloile <tle36@huskers.unl.edu>
2020-08-20 17:37:58 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Thanhloi Le
+ * Date: 2020/08/18
+ * A simple hello world program in C
+ **/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Thanhloi Le\n");
+  printf("Electrical Engineering\n");
+
+  return 0;
+}
